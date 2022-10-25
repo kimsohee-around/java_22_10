0fcba8444c6d661f899616079e409f38f0a9a5b5
SOHEE kim <koreait.sec2020@gmail.com>
2022-10-25 14:59:47 +0900

    java1025

---
+++ part1/src/koreait/day04/C24EmailValid.java
@@ -0,0 +1,68 @@
+package koreait.day04;
+
+import java.util.Scanner;
+
+public class C24EmailValid {
+	//사용자가 입력한 이메일(문자열)을 검사하기.
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("이메일을 입력하세요. >>> ");
+		String email = sc.nextLine();			//문자열 입력 nextLine()
+		
+		System.out.println("email : " + email);
+		
+		//0. @ 문자가 있는가?   ->   @ 문자가 없으면 리턴값 : -1
+		System.out.println("@문자 있는지 확인 : " + email.indexOf('@'));
+		int idx = email.indexOf('@');
+		
+		//1.@ 는 한번만 입력했는가?  
+		System.out.println("@문자가 1개인지 확인 :" + (email.indexOf('@')==email.lastIndexOf('@')));
+		
+		//2. (1번이 참)@를 기준으로 앞의 문자열 추출하기
+		System.out.println("@ 문자 앞 계정이름 추출 : " + email.substring(0, idx));
+		
+		//3. @를 기준으로 뒤의 문자열 추출하기
+		System.out.println("@ 문자 뒤 도메인주소 추출 : " + email.substring(idx+1));
+		
+		//4. .com으로 끝나는가?
+		System.out.println(".com 으로 끝나는가? " + email.endsWith(".com"));
+		
+		//5. 시작문자가 알파벳인가? - charAt(0) 첫번째 문자를 검사
+		char ch0 = email.charAt(0);
+		System.out.println("시작문자가 알파벳인가 ? " + 
+					(ch0>='A' && ch0 <='Z' || ch0 >='a' && ch0 <='z'));
+		
+		System.out.println("시작문자가 알파벳인가(메소드) ? " + isAlphabet(ch0));
+		
+		//6. 2번에서 추출한 계정이름이 5글자 이상인지?
+		System.out.println("계정이름이 5글자 이상인지?" + (email.substring(0, idx).length() > 5));
+		
+		
+		//문제 : 입력된 이메일이 사용가능한지 체크 결과 출력하기
+		//요구사항 : 1,5,6이 참이고 , '3번에서 . 문자가 1개 이상' 등 4가지 모두 만족하면 사용가능한 이메일
+		//		   사용할수 없는 이메일 일때는 이유를 알려주기.
+		boolean result;	
+		
+		
+		
+		
+		
+		if(result) {
+			System.out.println("입력하신 메일주소는 사용가능합니다.");
+		}else {
+			System.out.println("이유 : ");    //불가능 사유는 꼭 여기서 출력하지 않아도 됩니다.
+			System.out.println("입력한 메일주소는 사용불가능 합니다.");
+		}
+		
+		
+		
+	}
+
+	public static boolean isAlphabet(char ch0) {
+		return (ch0>='A' && ch0 <='Z' || ch0 >='a' && ch0 <='z');
+	}
+	
+	
+}
