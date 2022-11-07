9ad37c8a7cf5296a0f179f4c8532d4ad611c1fea
SOHEE kim <koreait.sec2020@gmail.com>
2022-11-07 09:51:37 +0900

    java1107

--- part2/src/koreait/day13/Cat.java
+++ part2/src/koreait/day13/Cat.java
@@ -3,13 +3,14 @@ package koreait.day13;
 public class Cat extends Animal{
 	
 	public Cat() {
-		//생략된 실행명령은? super() 입니다.
+		//생략된 실행명령은? super() 입니다.부모클래스 생성자가 실행됩니다.
 		System.out.println("냐용이가 생성되었습니다.");
 	}
 	
 	@Override
-	public void sound() {
-		System.out.println("😺😺😺😺야옹야옹 웁니다." + getName());
-	}
+	public void sound() {		//부모클래스의 인스턴스 메소드를 재정의(오버라이드) 
+		System.out.println("😺😺😺😺야옹야옹 웁니다." + getName());   //private 필드 name 값 가져오는 getter
+		System.out.println("color : " + getColor());
+	} 
 	
 }
--- part2/src/koreait/day13/Puppy.java
+++ part2/src/koreait/day13/Puppy.java
@@ -3,7 +3,7 @@ package koreait.day13;
 public class Puppy extends Animal{
 	private String type;
 	
-	//생성자 생략 가능합니다. -default 생성자 super() 호출합니다.
+	//생성자 생략 가능합니다. -default 생성자 super() 호출합니다.부모클래스 생성자가 실행됩니다.
 	
 	public String getType() {
 		return type;
@@ -13,12 +13,25 @@ public class Puppy extends Animal{
 		this.type = type;
 	}
 	
-	public void test() {
-		System.out.println("Puppy : test 메소드");
+	public void test() {		//Puppy 클래스의 인스턴스 메소드
+		System.out.println("Puppy : 견종 - " + type);
 	}
 	
-	@Override //@ 어노테이션은 명령실행에 도움을 주는 주석입니다.
+	@Override //@ 어노테이션은 명령실행에 도움을 주는 주석입니다.- 오버라이드는 부모의 인스턴스메소드 이름과 일치하지 않으면 오류
 	public void sound() {
 		System.out.println("🐶🐶🐶 멍멍 합니다.");
 	}
 }
+
+class Frog extends Animal{
+	
+	public static void jump() {
+		System.out.println("개구리 점프합니다.");
+	}
+}
+
+
+
+
+
+
