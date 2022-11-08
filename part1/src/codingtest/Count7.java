b243efc32ba0bbd3cd8911cf3635c1d6b979e76d
SOHEE kim <koreait.sec2020@gmail.com>
2022-11-08 11:46:12 +0900

    java1107

--- part1/src/codingtest/Count7.java
+++ part1/src/codingtest/Count7.java
@@ -1,7 +1,6 @@
 package codingtest;
 
 import java.util.Arrays;
-
 public class Count7 {
 	public static void main(String[] args) {
 		int[] array1 = new int[] { 7, 77, 17 };
@@ -17,29 +16,29 @@ public class Count7 {
 	public static int solution(int[] array) {
         int answer = 0;
         for(int i=0;i<array.length;i++){
-            String temp=String.valueOf(array[i]);
+            String temp=String.valueOf(array[i]);	//정수를 문자열로 변환
             for(int j=0;j<temp.length();j++)
                 if(temp.charAt(j)=='7') answer++;
         }
         return answer;
     }
-
+	//참고 : 문자열을 정수로 변환 Integer.parseInt(문자열)
 	// 7의 갯수
 	public static int solution1(int[] array) {	//김지연
 			int answer = 0;
 			for (int a : array) {
 				while (a != 0) {
-					if (a % 10 == 7)			answer++;
+					if (a % 10 == 7) answer++;
 					a /= 10;
 				}
 			}
 			return answer;
 	}
-		
+	
+	//{ 7, 77, 17 }를 "77717" 문자열로 변화해서 7의 개수 세기
 	public static int solution2(int[] array) {  //오준석
 			int answer = 0;
 			String num = "";
-
 			for (int i = 0; i < array.length; i++) {
 				num += array[i];
 //	        	System.out.println(num);
@@ -47,8 +46,7 @@ public class Count7 {
 			for (int j = 0; j < num.length(); j++) {
 				char ch = num.charAt(j);
 //	        	System.out.println(ch);
-				if (ch == '7')
-					answer++;
+				if (ch == '7') 	answer++;
 			}
 
 			return answer;
