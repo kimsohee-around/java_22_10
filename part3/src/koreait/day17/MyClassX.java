1e651a1163b5c413143a6e02f0f670bb5fd0e799
SOHEE kim <koreait.sec2020@gmail.com>
2022-11-11 11:37:41 +0900

    java1111

--- part3/src/koreait/day17/MyClassX.java
+++ part3/src/koreait/day17/MyClassX.java
@@ -1,8 +1,9 @@
 package koreait.day17;
 
-//InterfaceA의 구현체(구현클래스,실체)
+//InterfaceA의 구현체(구현클래스,실체)   : implements
 public class MyClassX implements InterfaceA{
 
+	//InterfaceA 의 추상메소드를 재정의 해야 합니다. : 하나로도 빠지면 오류
 	@Override
 	public void methodA() {
 		System.out.println("MyClassX methodA");
@@ -14,5 +15,8 @@ public class MyClassX implements InterfaceA{
 		System.out.println("MyClassX methodB :" + num);
 		return 999;
 	}
+	
+	
+	//InterfaceA 의 디폴트메소드는 필요한 경우 재정의 합니다.
 
 }
