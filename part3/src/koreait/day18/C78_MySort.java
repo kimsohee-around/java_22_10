447e205897e87023d87a7cd8efacf8a3e4f66978
SOHEE kim <koreait.sec2020@gmail.com>
2022-11-14 15:50:21 +0900

    java1114

--- part3/src/koreait/day18/C78_MySort.java
+++ part3/src/koreait/day18/C78_MySort.java
@@ -2,16 +2,16 @@ package koreait.day18;
 
 import java.util.Arrays;
 
-//작성자 강화민
+
 public class C78_MySort {
 
 	public static void main(String[] args) {
 		
-		//정렬 알고리즘(오름차순)
+		//정렬 알고리즘(오름차순) : 비교와 교환
 		int[] nums = {34, 77, 19, 56, 45, 9};
 		
-		for(int i = 0; i < nums.length-1; i++) {	//마지막 i = nums.length-2
-			for(int k =i+1; k < nums.length; k++) {
+		for(int i = 0; i < nums.length; i++) {	//마지막 i = nums.length-2
+			for(int k =i+1; k < nums.length; k++) {   //k는 i 보다 뒤에 있는 인덱스
 				if(nums[i] > nums[k]) {		// 인덱스 i와 k 위치의 값을 교환하기
 					int temp = nums[i];
 					nums[i] = nums[k];
