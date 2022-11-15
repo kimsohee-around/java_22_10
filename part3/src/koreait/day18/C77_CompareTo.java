9e0930c10a66356df5e8c832fda3fd3197f01c4f
SOHEE kim <koreait.sec2020@gmail.com>
2022-11-15 10:03:30 +0900

    java1115

--- part3/src/koreait/day18/C77_CompareTo.java
+++ part3/src/koreait/day18/C77_CompareTo.java
@@ -11,11 +11,13 @@ public class C77_CompareTo {
 		na="mi";
 		
 		//String은 알파벳(사전식)비교    : 대문자 < 소문자
+		//compareTo 메소드 : 문자열 2개를 각각 같은위치 문자하나씩 아스키코드값으로 비교     k,l,m
 		System.out.println("kim과 momo : " + na.compareTo(you));  //-2(음수) : "kim" < "momo", "kim"- "momo"<0
 		System.out.println("kim과 Son : " + na.compareTo(he));	//24(양수)  : "kim" >  "Son"
 
 		int a =12 , b=45;
 		System.out.println("a-b :" + (a-b));    //12-45 <0
+		//a-b< 0 이라면 a<b , a-b>0 이라면 a>b  와 같이 뺼셈식으로 2개의 값을 비교.(int,double,char...)
 		
 		Integer aa=12;  
 		Integer bb=45;
