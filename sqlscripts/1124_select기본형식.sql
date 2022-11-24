d92d9fd2d5f13ba698dc80a352580c276eb7e1a1
SOHEE kim <koreait.sec2020@gmail.com>
2022-11-24 13:54:31 +0900

    sql1124

--- sqlscripts/1124_select기본형식.sql
+++ sqlscripts/1124_select기본형식.sql
@@ -4,7 +4,7 @@ CREATE TABLE tbl_member (
 	name varchar2(50),   -- 이름
 	email varchar2(100),  -- 이메일
 	join_date DATE			-- 가입날짜 , 날짜형식 : 날짜 패턴 문자열이 자동으로 날짜형식 변환
-							--   yyyy-mm-dd HH:MM:SS.ms
+							-- 출력형식 :  yyyy-mm-dd HH:MM:SS.ms
 );
 
 -- 예제 데이터
@@ -15,6 +15,8 @@ INSERT INTO TBL_MEMBER VALUES (4,'박사나','san@gmail.com','2022-10-05');
 INSERT INTO TBL_MEMBER VALUES (5,'이나연','nayeon@gmail.com','2020-10-05');
 INSERT INTO TBL_MEMBER VALUES (6,'강나현','nahyeon@gmail.com','2021-05-05');
 INSERT INTO TBL_MEMBER VALUES (7,'나쯔위','na1234@gmail.com','2021-05-05');
+INSERT INTO TBL_MEMBER(MNO,NAME,JOIN_DATE) VALUES (8,'강감찬','2020-05-05');
+INSERT INTO TBL_MEMBER(MNO,NAME,JOIN_DATE) VALUES (9,'이순신','2020-05-05');
 
 
 -- select 기본형식
@@ -36,15 +38,24 @@ SELECT * FROM tbl_member tm WHERE name LIKE '%나%';
 
 -- 3) 여러가지 값으로 조회 : 이름이 김모모 또는 박나연
 SELECT * FROM TBL_MEMBER tm WHERE name ='김모모' OR name ='박나연';
-SELECT * FROM TBL_MEMBER tm WHERE name ='김모모' AND name ='박나연';
+SELECT * FROM TBL_MEMBER tm WHERE name IN ('김모모','박나연');		-- OR 연산에 해당
+SELECT * FROM TBL_MEMBER tm WHERE name NOT IN ('김모모','박나연');		
 
+SELECT * FROM TBL_MEMBER tm WHERE name ='김모모' AND name ='박나연';   -- 2가지 값 동시 만족
 
+-- 4) 이메일 email 컬럼의 null값 조회
+SELECT * FROM TBL_MEMBER tm WHERE EMAIL IS NULL ;
+SELECT * FROM TBL_MEMBER tm WHERE EMAIL IS NOT NULL ;
 
+-- 수치값 또는 날짜 컬럼을 조건식으로 한다면 관계식 : > , < , >= , <= , =
+-- 예시 : 1) mno 컬럼값이 5 조회   2) mno 컬럼 값이 4미만 조회   3) mno 컬럼값 1,2,5,7 조회
 
+SELECT * FROM TBL_MEMBER tm WHERE mno =5;	-- mno는 number(수치:정수,실수) 타입
+SELECT * FROM TBL_MEMBER tm WHERE mno ='5';		-- 자동 캐스팅 : 문자열에서 number로	
+SELECT * FROM TBL_MEMBER tm WHERE mno ='abc';		-- 오류 :자동 캐스팅 못하는 문자열
 
-
-
-
+SELECT * FROM TBL_MEMBER tm WHERE mno <4;
+SELECT * FROM TBL_MEMBER tm WHERE mno IN (1,2,5,7);
 
 
 
---
+++ sqlscripts/1124_제약조건(1).sql
@@ -0,0 +1,54 @@
+-- 제약 조건 : 테이블에 부적절한 데이터가 저장되는 것을 방지.
+--			 저장할 데이터의 규칙을 적용. constraint(제약)
+-- 1) NOT NUll  2)UNIQUE (유일한)   3) PRIMARY KEY
+
+CREATE TABLE tbl_const1(
+	mno number(5),
+	id number(5) NOT NULL    -- NOT NULL 제약조건 컬럼에 적용
+);
+
+INSERT INTO tbl_const1 VALUES (100,999);
+
+SELECT * FROM tbl_const1;
+
+-- 제약조건 적용
+INSERT INTO tbl_const1(mno) VALUES (200);    -- 오류 : id컬럼값은 필수 입력 항목
+INSERT INTO tbl_const1(id) VALUES (998);
+
+-- 2) UNIQUE 제약조건 : null 은 허용, 동일한 컬럼에 동일한 값은 저장 못함.
+CREATE TABLE tbl_const2(
+	mno number(5),
+	id number(5) UNIQUE 		
+);
+INSERT INTO tbl_const2 VALUES (100,999);
+INSERT INTO tbl_const2(mno) VALUES (200);    
+INSERT INTO tbl_const2(id) VALUES (998);
+-- 같은 값을 한번더 insert 
+INSERT INTO tbl_const2(mno) VALUES (200);
+INSERT INTO tbl_const2(id) VALUES (998);		-- 오류 : 
+INSERT INTO tbl_const2 VALUES (100,999);
+
+SELECT * FROM tbl_const2;
+
+-- 3) 기본키 : 테이블의 row(행)를 구별합니다. not null 과 unique 가 적용
+--  참고 : default 로 기본값을 저장합니다.(값을 지정안했을 때)
+CREATE TABLE tbl_member2 (
+	mno NUMBER(5) PRIMARY KEY ,    
+	name varchar2(50) NOT NULL ,   -- 이름
+	email varchar2(100) UNIQUE ,  -- 이메일
+	join_date DATE	DEFAULT sysdate   -- sysdate : 오라클 함수(현재날짜/시간)		
+);
+-- 테이블에서 기본키 컬럼을 설정하는 것은 필수.
+
+INSERT INTO TBL_MEMBER2 VALUES (1,'김모모','momo@naver.com','2022-11-24');
+INSERT INTO TBL_MEMBER2 VALUES (1,'박나연','parkny@gmail.com','2022-10-24');  -- 오류
+INSERT INTO TBL_MEMBER2 VALUES (3,NULL,'slgichoi@naver.com','2021-09-24');   -- 오류
+INSERT INTO TBL_MEMBER2 VALUES (4,'박사나','parkny@gmail.com','2022-10-05');
+INSERT INTO TBL_MEMBER2 VALUES (5,'이나연','parkny@gmail.com','2020-10-05');   -- 오류
+INSERT INTO TBL_MEMBER2(mno,name) VALUES (6,'강쯔위');     -- 필수 입력컬럼만 저장
+SELECT * FROM TBL_MEMBER2 tm ;
+
+
+
+
+
