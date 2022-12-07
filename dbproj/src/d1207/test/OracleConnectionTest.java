e5fe8cadc268388d9c2e3e60623c055d3c6f5056
SOHEE kim <koreait.sec2020@gmail.com>
2022-12-07 11:05:32 +0900

    jdbc1207

--- dbproj/src/d1207/test/OracleConnectionTest.java
+++ dbproj/src/d1207/test/OracleConnectionTest.java
@@ -11,8 +11,9 @@ public class OracleConnectionTest {
 		Connection conn = null;			//Connection 인터페이스
 
 		//db접속 정보
-		String url = "jdbc:oracle:thin:@192.168.1.254:1521:XE";   //데이터베이스 URL - 디비버에서 확인가능합니다. jdbc:oracle:thin 는 지정된 프로토콜
-					//jdbc프로토콜,오라클 지정 ,localhost는 원격서버에 접속시 ip로 대체 , 1521은 오라클의 포트(서비스 구별값)
+		String url = "jdbc:oracle:thin:@192.168.1.254:1521:XE";   
+					//데이터베이스 URL - 디비버에서 확인가능합니다. jdbc:oracle:thin 는 지정된 프로토콜
+					//				 localhost는 원격서버에 접속시 ip로 대체 , 1521은 오라클의 포트(서비스 구별값)
 		String driver="oracle.jdbc.driver.OracleDriver";     //오라클 드라이버클래스(패키지명.클래스명) - 외부라이브러리 ojdbc6.jar 필요
 		String user ="iclass10";			//접속할 계정 정보 - 계정이름
 		String password="1234";				//            ㄴ 패스워드
