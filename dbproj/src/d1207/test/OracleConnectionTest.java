14aa6357a418a890245099ce168433c4e39a65da
SOHEE kim <koreait.sec2020@gmail.com>
2022-12-07 13:27:42 +0900

    jdbc1207

--- dbproj/src/d1207/test/OracleConnectionTest.java
+++ dbproj/src/d1207/test/OracleConnectionTest.java
@@ -14,7 +14,8 @@ public class OracleConnectionTest {
 		String url = "jdbc:oracle:thin:@192.168.1.254:1521:XE";   
 					//데이터베이스 URL - 디비버에서 확인가능합니다. jdbc:oracle:thin 는 지정된 프로토콜
 					//				 localhost는 원격서버에 접속시 ip로 대체 , 1521은 오라클의 포트(서비스 구별값)
-		String driver="oracle.jdbc.driver.OracleDriver";     //오라클 드라이버클래스(패키지명.클래스명) - 외부라이브러리 ojdbc6.jar 필요
+		String driver="oracle.jdbc.driver.OracleDriver";     
+					//오라클 드라이버클래스(패키지명.클래스명) - 외부라이브러리 ojdbc6.jar 필요
 		String user ="iclass10";			//접속할 계정 정보 - 계정이름
 		String password="1234";				//            ㄴ 패스워드
 		
@@ -22,7 +23,7 @@ public class OracleConnectionTest {
 	//1.메모리에 데이터베이스 드라이버 클래스를 로드합니다.(로드-> 적재)  forName() 메소드는 throws ClassNotFoundException 처리. 
 			Class.forName(driver);
 			
-	//2. DriverManager 클래스는 메모리에 로드된 드라이버 클래스를 관리하고		
+	//2. DriverManager 클래스는 메모리에 로드된 드라이버 클래스를 관리하고	, 드라이버 클래스의 종류(Oracle,MySql 등등) 에 따라 알아서	
 	//3. DriverManager 클래스의 메소드 getConnection 으로 데이터베이스(오라클) 드라이버에 대한 Connection 구현객체를 생성
 	//                            		ㄴ 인터페이스 Connection 타입으로 참조합니다.
 	//									ㄴ new 연산 외에 객체를 생성하는 다른 방법. : 메소드를 실행하여 객체생성하는 디자인패턴	
