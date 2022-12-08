59d5fce16c7688b4afdd2ba104e33dfb7b7fbf56
SOHEE kim <koreait.sec2020@gmail.com>
2022-12-08 14:29:44 +0900

    jdbc1208

--- dbproj/src/d1208/test/MemberDao.java
+++ dbproj/src/d1208/test/MemberDao.java
@@ -11,14 +11,15 @@ import jdbc.util.OracleUtil;
 
 //DAO 클래스 : Data Access Object . SQL 실행하는 메소드를 모아놓은 클래스
 //				ㄴ DAO 클래스는 `객체를 1개만 생성되도록하는 디자인패턴` - 싱글턴(singleton) 으로 만듭니다.
-//				ㄴ 1개의 객체로 여러 프로그램에서 사용하도록 합니다. static 안쓰고 싱글턴으로 만듭니다.
+//				ㄴ 1개의 객체로 여러 프로그램에서 사용하도록 합니다. static 메소드 안쓰고 싱글턴으로 만듭니다.
+//				ㄴ 인스턴스 메소드로 만들어야 인터페이스 적용과 상속에서 메소드 재정의가 가능합니다.
 public class MemberDao {
 	
-	
-	
-	
-	
-	
+	private static MemberDao dao = new MemberDao();    //private static 멤버변수가 참조하는 것은 자기자신 객체
+	private MemberDao() { };				//생성자 private(이 클래스 외에는 사용못함.)
+	public static MemberDao getMemberDao() {		//생성된 객체를 리턴해 주는 public 메소드 - 항상 동일한 객체를 리턴
+		return dao;
+	}
 	
 	public void insert(Member member) throws SQLException {
 		//Member 객체를 인자로 받아서 insert 할 값으로 SQL에 전달.
