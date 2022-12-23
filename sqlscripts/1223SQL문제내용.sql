bc259446d40d10aa14c5f38567f596334f311ea6
kimsewhee <koreait.sec2020@gmail.com>
2022-12-23 11:21:13 +0900

    sql평가문제

---
+++ sqlscripts/1223SQL문제내용.sql
@@ -0,0 +1,80 @@
+CREATE TABLE TBL_CUSTOM# (
+	CUSTOM_ID VARCHAR2(20) PRIMARY KEY, -- 최대 20byte의 가변 문자열, NOT NULL AND UNIQUE
+	NAME NVARCHAR2(20) NOT NULL, -- 최대 20개 문자의 가변 문자열
+	EMAIL VARCHAR2(30),
+	AGE NUMBER(3),
+	REG_DATE timestamp
+);
+
+SELECT * FROM TBL_CUSTOM# tc ;
+
+INSERT INTO TBL_CUSTOM# (CUSTOM_ID, NAME, EMAIL, AGE, REG_DATE)
+VALUES ('mina012', '김미나', 'kimm@gmail.com', 20, to_date('2022-02-07 15:03:06','YYYY-MM-DD HH24:MI:SS'));
+INSERT INTO TBL_CUSTOM# (CUSTOM_ID, NAME, EMAIL, AGE, REG_DATE)
+VALUES ('hongGD', '홍길동', 'gil@korea.com', 32, to_date('2022-02-07 15:03:06','YYYY-MM-DD HH24:MI:SS'));
+INSERT INTO TBL_CUSTOM# (CUSTOM_ID, NAME, EMAIL, AGE, REG_DATE)
+VALUES ('twice', '박모모', 'momo@daum.net', 39, to_date('2021-12-25 00:00:00','YYYY-MM-DD HH24:MI:SS'));
+INSERT INTO TBL_CUSTOM# (CUSTOM_ID, NAME, EMAIL, AGE, REG_DATE)
+VALUES ('wonder', '이나나', 'nana@korea.kr', 23, '2022-02-05');
+INSERT INTO TBL_CUSTOM# (CUSTOM_ID, NAME, EMAIL, AGE, REG_DATE)
+VALUES ('sana', '최사나', 'unknown', 22, '2022-02-09 15:19:24');
+
+-- 상품 테이블 : 카테고리 예시 A1:전자제품, B1:식품
+CREATE TABLE TBL_PRODUCT#(
+	PCODE VARCHAR2(20) PRIMARY KEY,
+	CATEGORY CHAR(2) ,
+	PNAME NVARCHAR2(20) NOT NULL,
+	PRICE NUMBER(9) NOT NULL
+);
+INSERT INTO TBL_PRODUCT# (PCODE, CATEGORY, PNAME, PRICE)
+VALUES ('CJ-BABQ1', 'B1', 'CJ햇반잡곡밥SET', 26000);
+INSERT INTO TBL_PRODUCT# (PCODE, CATEGORY, PNAME, PRICE)
+VALUES ('DOWON123a', 'B1', '동원참치선물세트', 54000);
+INSERT INTO TBL_PRODUCT# (PCODE, CATEGORY, PNAME, PRICE)
+VALUES ('dk_143', 'A2', '모션데스크', 234500);
+INSERT INTO TBL_PRODUCT# (PCODE, CATEGORY, PNAME, PRICE)
+VALUES ('IPAD011', 'A1', '아이패드10', 880000);
+INSERT INTO TBL_PRODUCT# (PCODE, CATEGORY, PNAME, PRICE)
+VALUES ('GAL0112', 'A1', '갤럭시20', 912300);
+INSERT INTO TBL_PRODUCT# (PCODE, CATEGORY, PNAME, PRICE)
+VALUES ('CHR-J59', 'A2', 'S체어', 98700);
+select * from tbl_product#;
+
+--구매 테이블 : 어느 고객이 무슨 상품을 구입하는가?
+CREATE TABLE TBL_BUY#(
+    buy_seq number(8) primary key,
+	CUSTOM_ID VARCHAR2(20) NOT NULL,
+	PCODE VARCHAR2(20) NOT NULL,
+	QUANTITY NUMBER(5) NOT NULL, -- 수량
+	BUY_DATE timestamp default sysdate,
+    foreign key (custom_id) references tbl_custom#(custom_id),
+    foreign key (pcode) references tbl_product#(pcode)
+);
+create sequence buy_seq;
+INSERT INTO TBL_BUY# (buy_seq,CUSTOM_ID, PCODE, QUANTITY, BUY_DATE)
+VALUES (buy_seq.nextval,'mina012', 'IPAD011', 1, to_date('2022-02-06 00:00:00','YYYY-MM-DD HH24:MI:SS'));
+INSERT INTO TBL_BUY# (buy_seq,CUSTOM_ID, PCODE, QUANTITY, BUY_DATE)
+VALUES (buy_seq.nextval,'hongGD', 'IPAD011', 2, to_date('2022-02-08 15:55:08','YYYY-MM-DD HH24:MI:SS'));
+INSERT INTO TBL_BUY# (buy_seq,CUSTOM_ID, PCODE, QUANTITY, BUY_DATE)
+VALUES (buy_seq.nextval,'wonder', 'DOWON123a', 3, to_date('2022-02-06 00:00:00','YYYY-MM-DD HH24:MI:SS'));
+INSERT INTO TBL_BUY# (buy_seq,CUSTOM_ID, PCODE, QUANTITY, BUY_DATE)
+VALUES (buy_seq.nextval,'mina012', 'dk_143', 1, '2022-02-08 15:55:08');
+INSERT INTO TBL_BUY# (buy_seq,CUSTOM_ID, PCODE, QUANTITY, BUY_DATE)
+VALUES (buy_seq.nextval,'twice', 'DOWON123a', 2, to_date('2022-02-07 08:49:55','YYYY-MM-DD HH24:MI:SS'));
+INSERT INTO TBL_BUY# (buy_seq,CUSTOM_ID, PCODE, QUANTITY, BUY_DATE)
+VALUES (buy_seq.nextval,'hongGD', 'dk_143', 1, '2022-02-11');
+INSERT INTO TBL_BUY# (buy_seq,CUSTOM_ID, PCODE, QUANTITY, BUY_DATE)
+VALUES (buy_seq.nextval,'twice', 'CHR-J59', 2, '2022-02-12');
+INSERT INTO TBL_BUY# (buy_seq,CUSTOM_ID, PCODE, QUANTITY, BUY_DATE)
+VALUES (buy_seq.nextval,'hongGD', 'CJ-BABQ1', 4, '2022-02-11');
+select * from tbl_buy#;
+-- 테이블 추가 (제조회사)
+create table tbl_copration(
+        copr_id number(4) primary key,      -- 제조사ID
+        title varchar(20) not null,
+        tel varchar(20) 
+);
+
+-- 상품테이블에 copr_id 컬럼을 추가(null 허용) (alter table)
+
+-- 상품테이블에 추가된 컬럼에 copr_id 대해 외래키 추가 (alter table)
---
+++ sqlscripts/1223조회SQL.sql
@@ -0,0 +1,36 @@
+-- 능력단위 과목명 :SQL 활용
+-- 문제2 제출자 : 김땡땡
+
+--1) 2022년 2월11일부터 그 이후에 판매된 상품 컬럼을 모두 조회 합니다.
+select * from tbl_buy#
+where buy_date > '2022-02-10';
+--2) 가장 가격이 높은 상품을 `상품코드,상품명,가격` 조회합니다.
+select pcode,pname,price 
+from tbl_product#
+where price = (select max(price) from tbl_product#);        -- 그룹(집계_함수)는 다른 컬럼과 함께 조회 할수 없음.
+
+--3) 상품의 카테고리별로 가장 비싼 상품을 `카테고리명, 최고금액` 으로 조회 합니다. category 컬럼 순서로 정렬하기.
+SELECT category, max(price) 
+FROM "TBL_PRODUCT#" tp 
+GROUP BY CATEGORY 
+ORDER BY CATEGORY ;
+
+--4) IPAD011 을 구매한 고객정보와 구매수량을 `고객ID, 이름,구매수량` 으로 조회합니다.
+SELECT tb.CUSTOM_ID ,name,quantity 
+FROM "TBL_CUSTOM#" tc ,"TBL_BUY#" tb 
+WHERE tc.CUSTOM_ID = tb.CUSTOM_ID       -- 조인 조건
+AND tb.PCODE = 'IPAD011';
+-- 또는 
+SELECT tb.CUSTOM_ID ,name,quantity 
+FROM "TBL_CUSTOM#" tc join "TBL_BUY#" tb 
+on tc.CUSTOM_ID = tb.CUSTOM_ID 
+AND tb.PCODE = 'IPAD011';       -- 외부 조인 할때는 조인 조건 아닌 경우 where 에 추가 조건을 작성하세요.
+
+--5) 날짜별로 총판매금액을 `날짜(년-월-일) , 총판매금액` 으로 조회합니다.
+SELECT buy_date2, sum(money)
+FROM
+(SELECT TO_CHAR(buy_date,'yyyy-MM-dd') buy_date2 ,tb.pcode,quantity * price  money
+        FROM TBL_BUY# tb ,TBL_PRODUCT# tp WHERE tb.pcode = tp.PCODE )   -- 서브쿼리
+GROUP BY buy_date2 ORDER BY buy_date2;
+
+
