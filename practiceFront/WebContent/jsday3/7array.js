a42bc8a574c32f0d78305f1fece97d963b36768d
kimsewhee <koreait.sec2020@gmail.com>
2022-12-29 08:50:20 +0900

    front1228

---
+++ practiceFront/WebContent/jsday3/7array.js
@@ -0,0 +1,79 @@
+/**
+ * 자바스크립트 배열
+ - 데이터 타입이 동일하지 않아도 됩니다.
+ - typeof 연산 결과는 object
+ - sort() 함수 : 오름 차순 정렬
+ - reverse() 함수 : 정렬이 아님. 현재 배열값 순서를 역으로 바꿈.
+ */
+ let arr1 = [11,22,44,99,55,66]				//global scope(전역 변수)
+ 
+ function print_arr1(){
+ 	document.getElementById('print1').innerHTML = arr1	//toString() 함수 실행결과
+    console.log(arr1)
+ }
+ 
+ function change_arr1() {
+ 	arr1=['java','html','css',3.14]       //let 변수 이므로 참조 배열 변경 가능.
+    document.getElementById('print4').innerHTML = arr1
+    console.log(arr1)
+ 
+ }
+
+const arr2 =['apple','mango','kiwi','banan','딸기','사과']				//const(상수) 식별자 선언
+  function print_arr2(){
+ 	document.getElementById('print6').innerHTML = arr2	//toString() 함수 실행결과
+    console.log(arr2)
+ }
+ 
+   function sort_arr2(){
+   	arr2.sort()
+ 	document.getElementById('print6').innerHTML = arr2	//toString() 함수 실행결과
+    console.log(arr2)
+ }
+ 
+    function reverse_arr2(){
+	   	arr2.reverse()
+	 	document.getElementById('print6').innerHTML = arr2	//toString() 함수 실행결과
+	    console.log(arr2)
+ }
+ 	function list_arr3() {
+ 			const arr3 = ['🧑사나','채영👩‍🦰','모모','나연','다현']
+            console.log(arr3)
+            let result ='<ul>'		
+            for(let i=0;i<arr3.length;i++){
+               result += '<li>' + arr3[i] +'</li>'		//문자열 연결연산. result=result+'hello'
+           	   result += `<li>${arr3[i]}</li>`
+            }
+            result += '</ul>'
+           document.getElementById('twice').innerHTML=result	
+ }
+ 	function foreach_arr3() {
+ 			const arr3 = ['🧑sana','채영👩‍🦰','momo','나연','다현']
+ 			 let result ='<ul>'	
+ 			arr3.forEach(function(value,index,array){		//배열변수.forEach(인자는 배열요소에 대해 각각 실행할 함수)
+ 													// 인자로 사용되는 함수(콜백callback 함수)는 함수이름 없이 사용
+ 													// 함수 인자 value,index,array는 배열에서 실행될때 정해진 순서
+ 				result += `<li>${value}(${index})</li>`			 //${}는 변수값을 출력기호						
+ 				console.log(`<li>${value}(${index}/${array.length})</li>`)
+ 			})
+ 	
+ 		   result += '</ul>'
+           document.getElementById('twicee').innerHTML=result	
+ 			
+ 	
+ 	
+ 	
+ 	}
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
